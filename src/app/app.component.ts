408dcdb4167ea4c295b95ce75a0f9859b5a782e8
NikhilKashyap22 <kashyapnikhil738@gmail.com>
2025-02-10 13:59:42 +0530

    Final codebase updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { FooterComponent } from "./shared/components/footer/footer.component";
 import { AddDoctorComponent } from './doctors/components/add-doctor/add-doctor.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { CreateScheduleComponent } from "./doctor-schedule/components/create-schedule/create-schedule.component";
+import { LoginComponent } from "./authentications/components/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, SidebarComponent, MainComponent, FooterComponent, CreateScheduleComponent],
+  imports: [RouterModule, HeaderComponent, SidebarComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/doctor-schedule/doctor-schedule-home/doctor-schedule-home.component.ts
+++ src/app/doctor-schedule/doctor-schedule-home/doctor-schedule-home.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 import { GetAllSchedulesComponent } from "../components/get-all-schedules/get-all-schedules.component";
-import { GetScheduleByIdComponent } from "../components/get-schedule-by-id/get-schedule-by-id.component";
-import { CreateScheduleComponent } from "../components/create-schedule/create-schedule.component";
-import { UpdateDoctorScheduleComponent } from "../components/update-doctor-schedule/update-doctor-schedule.component";
-import { HeaderComponent } from "../../shared/components/header/header.component";
-import { SidebarComponent } from "../../shared/components/sidebar/sidebar.component";
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-doctor-schedule-home',
   standalone: true,
-  imports: [GetAllSchedulesComponent, GetScheduleByIdComponent, CreateScheduleComponent, UpdateDoctorScheduleComponent, HeaderComponent, SidebarComponent],
+  imports: [GetAllSchedulesComponent],
   templateUrl: './doctor-schedule-home.component.html',
   styleUrl: './doctor-schedule-home.component.css'
 })
